d61fad58ed19c32e92e38bfbe8ecc4aa1b4b652a
ih551311 <ih551311@gmail.com>
2026-04-11 18:30:13 +0900

    [feat] - 캐릭터 애니메이션 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator myAnimator;
+
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public Transform groundCheck;
@@ -15,19 +17,51 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        myAnimator = GetComponent<Animator>();  
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        if(moveInput > 0)
+        {
+            transform.localScale = new Vector3(0.18f, 0.18f, 0.18f);
+        }
+        else if(moveInput < 0)
+        {
+            transform.localScale = new Vector3(-0.18f, 0.18f, 0.18f);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (isGrounded)
+        {
+            myAnimator.SetBool("jump", false);
+        }
+        else
+        {
+            myAnimator.SetBool("jump", true);
+        }
     }
 
     public void OnMove(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
         moveInput = input.x;
+        myAnimator.SetBool("move", true);
+        if(moveInput != 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
     }
 
     public void OnJump(InputValue value)
